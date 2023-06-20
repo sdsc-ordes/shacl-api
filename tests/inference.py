d2e6803cdf1fbe62555e48721d1891be72fc0e31
caviri <carlosvivarrios@gmail.com>
2023-06-20 08:48:10 +0200

    Fixed error in inference

--- tests/inference.py
+++ tests/inference.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def runInferenceTest():
-    output = subprocess.run(["shaclinference.sh", "-datafile", "tests-files/inf_imagingID.ttl", "-shapesfile", "tests-files/inf_ImagingOntologyShapes.ttl"], stdout=subprocess.PIPE)
+    output = subprocess.run(["shaclinfer.sh", "-datafile", "tests-files/inf_imagingID.ttl", "-shapesfile", "tests-files/inf_ImagingOntologyShapes.ttl"], stdout=subprocess.PIPE)
 
     with open("inferenceTest.ttl", 'wb') as f: 
         f.write(output.stdout)
--- tests/validation.py
+++ tests/validation.py
@@ -14,3 +14,4 @@ def runFailedInferenceTest():
 
 
 runValidationTest()
+runFailedInferenceTest()
