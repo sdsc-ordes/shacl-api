89eddabb946616ea21d289ff55a64558cf2d2248
Stacy <pnastya5@mail.ru>
2024-09-09 22:22:26 +0500

    - fixed cors

--- app/main.py
+++ app/main.py
@@ -10,20 +10,11 @@ from rdflib.namespace import Namespace, NamespaceManager
 
 import json
 
-origins = [
-    "http://localhost:8051",
-    "http://localhost:8080",
-    "http://localhost:8000",
-    "http://127.0.0.1:8051",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:8000",
-]
-
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
