17e4384e2028deed8c3c2fbd1579f1901cf303bc
cmdoret <cyril.mattheydoret@gmail.com>
2025-02-21 19:51:32 +0100

    style: ruff format

--- src/shacl-api/server.py
+++ src/shacl-api/server.py
@@ -12,162 +12,184 @@ import json
 app = FastAPI()
 SHAPES_PATH = os.getenv("SHAPES_PATH", "shapesfile.ttl")
 
+
 @app.get("/")
 def index():
-    return {"title": "Hello, welcome to the SHACL API v0.0.5. Compatible with Ontology v0.8."}
+    return {
+        "title": "Hello, welcome to the SHACL API v0.0.5. Compatible with Ontology v0.8."
+    }
+
 
 @app.post("/validate-jsonld")
-async def validateJsonLD(item: Request,
-             jsonInput: bool | None = None, 
-             jsonOutput: bool | None = None,
-             verbose: bool | None = None):
-     
-     data = await item.json()
-     data = data["data"]
-     if verbose:
-          print(data)
+async def validateJsonLD(
+    item: Request,
+    jsonInput: bool | None = None,
+    jsonOutput: bool | None = None,
+    verbose: bool | None = None,
+):
+    data = await item.json()
+    data = data["data"]
+    if verbose:
+        print(data)
 
-     if jsonInput:
-          if verbose:
-               print("Assuming json")
-               print(f"Type: {type(data)}")
-               print("Cleaning newlines")
-          data = json.dumps(data)
-     else:
-          if verbose:
-               print("Assuming string")
-               print(f"Type: {type(data)}")
-               print("Cleaning newlines")
-          data = data.splitlines()
-          data = ' '.join(data)
+    if jsonInput:
+        if verbose:
+            print("Assuming json")
+            print(f"Type: {type(data)}")
+            print("Cleaning newlines")
+        data = json.dumps(data)
+    else:
+        if verbose:
+            print("Assuming string")
+            print(f"Type: {type(data)}")
+            print("Cleaning newlines")
+        data = data.splitlines()
+        data = " ".join(data)
 
-     # This is generating the datafile necesary to run inference.
-     graph = Graph()
-     graph.parse(data=data, format="json-ld")
-     
-     SCHEMA = Namespace("http://schema.org/")
-     graph.namespace_manager.bind('schema', SCHEMA, override=True, replace=True)
-     ttl_input = str(graph.serialize(destination='datafile.ttl',format='turtle'))
+    # This is generating the datafile necesary to run inference.
+    graph = Graph()
+    graph.parse(data=data, format="json-ld")
 
+    SCHEMA = Namespace("http://schema.org/")
+    graph.namespace_manager.bind("schema", SCHEMA, override=True, replace=True)
+    ttl_input = str(graph.serialize(destination="datafile.ttl", format="turtle"))
 
-     output = subprocess.run(["shaclvalidate.sh", "-datafile", "datafile.ttl", "-shapesfile", SHAPES_PATH], stdout=subprocess.PIPE)
+    output = subprocess.run(
+        ["shaclvalidate.sh", "-datafile", "datafile.ttl", "-shapesfile", SHAPES_PATH],
+        stdout=subprocess.PIPE,
+    )
 
-     os.remove("datafile.ttl")
+    os.remove("datafile.ttl")
 
-     ### Read and provide JSON-LD
-     graph = Graph()
-     graph.parse(data=output.stdout, format="turtle")
-     SCHEMA = Namespace("http://schema.org/")
-     graph.namespace_manager.bind('schema', SCHEMA, override=True, replace=True)
-     jsonld = str(graph.serialize(format='json-ld'))
+    ### Read and provide JSON-LD
+    graph = Graph()
+    graph.parse(data=output.stdout, format="turtle")
+    SCHEMA = Namespace("http://schema.org/")
+    graph.namespace_manager.bind("schema", SCHEMA, override=True, replace=True)
+    jsonld = str(graph.serialize(format="json-ld"))
 
-     if jsonOutput:
-          jsonld = json.loads(jsonld)
+    if jsonOutput:
+        jsonld = json.loads(jsonld)
 
-     return {"jsonldOutput": jsonld, 
-             "ttlOutput": output.stdout, 
-             "jsonldInput": data,
-             "ttlInput": ttl_input}
+    return {
+        "jsonldOutput": jsonld,
+        "ttlOutput": output.stdout,
+        "jsonldInput": data,
+        "ttlInput": ttl_input,
+    }
 
 
 @app.post("/inference-jsonld")
-async def inferenceJsonLD(item: Request, 
-                   jsonInput: bool | None = None, 
-                   jsonOutput: bool | None = None,
-                   verbose: bool | None = None):
-     
-     data = await item.json()
-     data = data["data"]
-     if verbose:
-          print(data)
+async def inferenceJsonLD(
+    item: Request,
+    jsonInput: bool | None = None,
+    jsonOutput: bool | None = None,
+    verbose: bool | None = None,
+):
+    data = await item.json()
+    data = data["data"]
+    if verbose:
+        print(data)
 
-     if jsonInput:
-          if verbose:
-               print("Assuming json")
-               print(f"Type: {type(data)}")
-               print("Cleaning newlines")
-          data = json.dumps(data)
-     else:
-          if verbose:
-               print("Assuming string")
-               print(f"Type: {type(data)}")
-               print("Cleaning newlines")
-          data = data.splitlines()
-          data = ' '.join(data)
- 
-     # This is generating the datafile necesary to run inference.
-     graph = Graph()
-     graph.parse(data=data, format="json-ld")
-     
-     SCHEMA = Namespace("http://schema.org/")
-     graph.namespace_manager.bind('schema', SCHEMA, override=True, replace=True)
-     ttl_input= str(graph.serialize(destination='datafile.ttl',format='turtle'))
+    if jsonInput:
+        if verbose:
+            print("Assuming json")
+            print(f"Type: {type(data)}")
+            print("Cleaning newlines")
+        data = json.dumps(data)
+    else:
+        if verbose:
+            print("Assuming string")
+            print(f"Type: {type(data)}")
+            print("Cleaning newlines")
+        data = data.splitlines()
+        data = " ".join(data)
 
-     output = subprocess.run(["shaclinfer.sh", "-datafile", "datafile.ttl", "-shapesfile", "SHAPES_PATH"], stdout=subprocess.PIPE)
+    # This is generating the datafile necesary to run inference.
+    graph = Graph()
+    graph.parse(data=data, format="json-ld")
 
-     os.remove("datafile.ttl")
+    SCHEMA = Namespace("http://schema.org/")
+    graph.namespace_manager.bind("schema", SCHEMA, override=True, replace=True)
+    ttl_input = str(graph.serialize(destination="datafile.ttl", format="turtle"))
 
-     ### Read and provide JSON-LD
-     graph = Graph()
-     graph.parse(data=output.stdout, format="turtle")
-     SCHEMA = Namespace("http://schema.org/")
-     graph.namespace_manager.bind('schema', SCHEMA, override=True, replace=True)
-     jsonld = str(graph.serialize(format='json-ld'))
+    output = subprocess.run(
+        ["shaclinfer.sh", "-datafile", "datafile.ttl", "-shapesfile", "SHAPES_PATH"],
+        stdout=subprocess.PIPE,
+    )
 
-     if jsonOutput:
-          jsonld = json.loads(jsonld)
+    os.remove("datafile.ttl")
 
-     return {"jsonldOutput": jsonld, 
-             "ttlOutput": output.stdout, 
-             "jsonldInput": data,
-             "ttlInput": ttl_input}
+    ### Read and provide JSON-LD
+    graph = Graph()
+    graph.parse(data=output.stdout, format="turtle")
+    SCHEMA = Namespace("http://schema.org/")
+    graph.namespace_manager.bind("schema", SCHEMA, override=True, replace=True)
+    jsonld = str(graph.serialize(format="json-ld"))
 
+    if jsonOutput:
+        jsonld = json.loads(jsonld)
+
+    return {
+        "jsonldOutput": jsonld,
+        "ttlOutput": output.stdout,
+        "jsonldInput": data,
+        "ttlInput": ttl_input,
+    }
 
 
 @app.post("/validate")
-def validate(datafile:str=Form(...), 
-             shapesfile:str=Form(...)):
- 
+def validate(datafile: str = Form(...), shapesfile: str = Form(...)):
     datafile = base64.b64decode(str.encode(datafile))
     shapesfile = base64.b64decode(str.encode(shapesfile))
 
     # TODO: use tempfile package
-    with open("datafile.ttl", 'wb') as f: 
-         f.write(datafile)
-    
-    with open("shapesfile.ttl", 'wb') as f: 
-         f.write(shapesfile)
+    with open("datafile.ttl", "wb") as f:
+        f.write(datafile)
 
-    output = subprocess.run(["shaclvalidate.sh", "-datafile", "datafile.ttl", "-shapesfile", "shapesfile.ttl"], stdout=subprocess.PIPE)
+    with open("shapesfile.ttl", "wb") as f:
+        f.write(shapesfile)
+
+    output = subprocess.run(
+        [
+            "shaclvalidate.sh",
+            "-datafile",
+            "datafile.ttl",
+            "-shapesfile",
+            "shapesfile.ttl",
+        ],
+        stdout=subprocess.PIPE,
+    )
 
     os.remove("datafile.ttl")
     os.remove("shapesfile.ttl")
-    with open("validationTest.ttl", 'wb') as f: 
-         f.write(output.stdout)
+    with open("validationTest.ttl", "wb") as f:
+        f.write(output.stdout)
 
     return {"output": output.stdout}
 
 
 @app.post("/inference")
-def inference(datafile:str=Form(...), 
-             shapesfile:str=Form(...)):
- 
+def inference(datafile: str = Form(...), shapesfile: str = Form(...)):
     datafile = base64.b64decode(str.encode(datafile))
     shapesfile = base64.b64decode(str.encode(shapesfile))
     print(datafile)
 
     # TODO: use tempfile package
-    with open("datafile.ttl", 'wb') as f: 
-         f.write(datafile)
-    
-    with open("shapesfile.ttl", 'wb') as f: 
-         f.write(shapesfile)
+    with open("datafile.ttl", "wb") as f:
+        f.write(datafile)
 
-    output = subprocess.run(["shaclinfer.sh", "-datafile", "datafile.ttl", "-shapesfile", "shapesfile.ttl"], stdout=subprocess.PIPE)
+    with open("shapesfile.ttl", "wb") as f:
+        f.write(shapesfile)
+
+    output = subprocess.run(
+        ["shaclinfer.sh", "-datafile", "datafile.ttl", "-shapesfile", "shapesfile.ttl"],
+        stdout=subprocess.PIPE,
+    )
 
     os.remove("datafile.ttl")
     os.remove("shapesfile.ttl")
-    with open("validationTest.ttl", 'wb') as f: 
-         f.write(output.stdout)
+    with open("validationTest.ttl", "wb") as f:
+        f.write(output.stdout)
 
     return {"output": output.stdout}
